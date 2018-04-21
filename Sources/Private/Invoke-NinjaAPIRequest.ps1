3f8bf37caf714a49350fff49774507b747f67eaf
Max Anderson <cctpmax@gmail.com>
2018-04-21 13:46:11 -0400

    Added in parameters, migrated the header, rest request, and error handling from Get-NinjaCustomer.

--- Sources/Private/Invoke-NinjaAPIRequest.ps1
+++ Sources/Private/Invoke-NinjaAPIRequest.ps1
@@ -16,7 +16,59 @@ Function Invoke-NinjaAPIRequest {
     [CmdletBinding()]
 
     Param (
-        
+
+        [String]$HTTPVerb,
+
+        [String]$Resource,
+
+        [String]$AccessKeyID,
+
+        [String]$SecretAccessKey,
+
+        [String]$BaseNinjaAPIURI = "https://api.ninjarmm.com"
+
     )
+
+    $Header = New-NinjaRequestHeader -HTTPVerb $HTTPVerb -Resource $Resource -AccessKeyID $AccessKeyID -SecretAccessKey $SecretAccessKey
+    
+    Try {
+    
+        $Rest = Invoke-RestMethod -Method $HTTPVerb -Uri ($BaseNinjaAPIURI + $Resource) -Headers $Header
+    
+    }
+
+    Catch {
+
+        Switch ($_.ErrorDetails.Message | ConvertFrom-JSON | Select-Object -ExpandProperty error_code) {
+            
+            2 {
+
+                Throw "Request header missing or malformed."
+
+            }
+
+            5 {
+
+                Throw "Unable to authenticate to the API. There is an issue with the validity of the API keys."
+
+            }
+            
+            6 {
+
+                Throw "Too many requests. List API requests are rate limited to 10 requests per 10 minutes by Ninja."
+
+            }
+
+            Default {
+
+                Throw $_
+
+            }
+
+        }
+
+    }
+    
+    Write-Output $Rest
 
 }
